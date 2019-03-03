2733d4a2bc0866986c78596576b18f02c5676a99
Ray2011ee <zhurui2011ee@163.com>
2019-03-03 17:26:52 +0800

    Typedef modify

--- typedef.h
+++ typedef.h
@@ -1,8 +1,8 @@
 #ifndef _TYPEDEF_H_
 #define _TYPEDEF_H_
 
-typedef unsigned int UINT32
-typedef int		     INT32
-typedef bool		 BOOL	
+typedef unsigned int    UINT32
+typedef int             INT32
+typedef bool            BOOL	
 
 #endif
