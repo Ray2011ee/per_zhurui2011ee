7f76222e94a0b23f3295ec7351a95213754afc68
Ray2011ee <zhurui2011ee@163.com>
2019-03-03 17:36:30 +0800

    Typedef Modify

--- typedef.h
+++ typedef.h
@@ -1,8 +1,14 @@
 #ifndef _TYPEDEF_H_
 #define _TYPEDEF_H_
 
-typedef unsigned int    UINT32
-typedef int             INT32
-typedef bool            BOOL	
+
+typedef unsigned long long  UINT64
+typedef unsigned int        UINT32
+typedef int                 INT32
+typedef unsigned short      UINT16
+typedef short               INT16
+typedef unsigned char       UINT8
+typedef char                INT8	
+typedef bool                BOOL
 
 #endif
